e9985aff24fc2616970efa87370c6e3f8bd9bad7
MrKenobii <anldygc35@windowslive.com>
2022-08-17 17:33:37 +0300

    Communication between OrderService and InventoryService

--- inventory-service/src/main/java/com/anilduyguc/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/anilduyguc/inventoryservice/service/InventoryService.java
@@ -1,17 +1,26 @@
 package com.anilduyguc.inventoryservice.service;
 
+import com.anilduyguc.inventoryservice.dto.InventoryResponse;
 import com.anilduyguc.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
-    @Transactional
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
     }
 }
