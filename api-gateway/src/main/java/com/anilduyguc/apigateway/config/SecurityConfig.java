41f25966ca31fdda2d9a2c797ba050cea523f40d
MrKenobii <anldygc35@windowslive.com>
2022-09-19 18:58:59 +0300

    Revert "Dockerizing all services"
    This reverts commit f8003284f72adc8cf06f3dc3cf951a7ee8cdde39.

--- api-gateway/src/main/java/com/anilduyguc/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/anilduyguc/apigateway/config/SecurityConfig.java
@@ -11,15 +11,16 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity
-                .csrf().disable()
-                .authorizeExchange(exchange ->
-                        exchange.pathMatchers("/eureka/**")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated())
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
         return serverHttpSecurity.build();
     }
 }
